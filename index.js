4dcd666afd7777ad60e993b8db17ebeba7534a81
Hadifa Rousheen <93543742+hadifarousheen@users.noreply.github.com>
2025-03-15 22:48:35 +0530

    changes in index

--- index.js
+++ index.js
@@ -21,82 +21,79 @@ const form=document.querySelector('.form');
 
 
 sname.addEventListener('input',(e)=>{
-   if(localStorage.getItem('student').sname===null){
+
    const snameuser=e.target.value;
    const studentobj=JSON.parse(localStorage.getItem('student'));
    studentobj.sname=snameuser;
    localStorage.setItem('student',JSON.stringify(studentobj));
-   }
-   else{
-      isregistered=true;
-   }
+  
 })
 
 htno.addEventListener('input',(e)=>{
-   if(localStorage.getItem('student').htno===null){
+
     const htuser=e.target.value;
     const studentobj=JSON.parse(localStorage.getItem('student'));
     studentobj.htno=htuser;
     localStorage.setItem('student',JSON.stringify(studentobj));
-   }
+   
  })
   
  course.addEventListener('input',(e)=>{
-   if(localStorage.getItem('student').course===null){
+
     const courseuser=e.target.value;
     const studentobj=JSON.parse(localStorage.getItem('student'));
     studentobj.course=courseuser;
     localStorage.setItem('student',JSON.stringify(studentobj));
-   }
+   
  })
 
  branch.addEventListener('input',(e)=>{
-   if(localStorage.getItem('student').branch===null){
+  
     const branchuser=e.target.value;
     const studentobj=JSON.parse(localStorage.getItem('student'));
     studentobj.branch=branchuser;
     localStorage.setItem('student',JSON.stringify(studentobj));
-   }
+   
  })
 
 
  year.addEventListener('input',(e)=>{
-   if(localStorage.getItem('student').year===null){
+
     const yearuser=e.target.value;
     const studentobj=JSON.parse(localStorage.getItem('student'));
     studentobj.year=yearuser;
     localStorage.setItem('student',JSON.stringify(studentobj));
-   }
+   
  })
 
 
  semester.addEventListener('input',(e)=>{
-   if(localStorage.getItem('student').semester===null){
+
     const semesteruser=e.target.value;
     const studentobj=JSON.parse(localStorage.getItem('student'));
     studentobj.semester=semesteruser;
     localStorage.setItem('student',JSON.stringify(studentobj));
-   }
+   
  })
 
 
  email.addEventListener('input',(e)=>{
-   if(localStorage.getItem('student').email===null){
+
     const emailuser=e.target.value;
     const studentobj=JSON.parse(localStorage.getItem('student'));
     studentobj.email=emailuser;
     localStorage.setItem('student',JSON.stringify(studentobj));
-   }
+   
  })
 
  rpasswords.addEventListener('input',(e)=>{
-   if(localStorage.getItem('student').rpassword===null){
+  
     const rpassworduser=e.target.value;
     const studentobj=JSON.parse(localStorage.getItem('student'));
     studentobj.rpassword=rpassworduser;
     localStorage.setItem('student',JSON.stringify(studentobj));
     localStorage.setItem('fine',0);
-   }
+   
  })
 
 
