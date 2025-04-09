b43e793b3c0bb19e0d6b47b604b94332d19fd39c
Hadifa Rousheen <93543742+hadifarousheen@users.noreply.github.com>
2025-04-09 21:09:15 +0530

    changes in form submit

--- index.js
+++ index.js
@@ -1,149 +1,81 @@
-const submit=document.querySelector('.register-submit');
+const submit = document.querySelector(".register-submit");
 let firstregistered;
-if (!localStorage.getItem('student')) {
-    let student = {};
-    firstregistered=false;
-    localStorage.setItem('student', JSON.stringify(student));
-  
+if (!localStorage.getItem("student")) {
+  let student = {};
+  firstregistered = false;
+  localStorage.setItem("student", JSON.stringify(student));
 }
 
-const sname=document.querySelector('.name');
-const htno=document.querySelector('.hallticketregister');
-const course=document.querySelector('.course');
-const branch=document.querySelector('.branch');
-const year=document.querySelector('.year');
-const semester=document.querySelector('.semester');
-const email=document.querySelector('.email');
-const rpasswords=document.querySelector('.rpassword');
-const login=document.querySelector('.loginsubmit');
+const sname = document.querySelector(".name");
+const htno = document.querySelector(".hallticketregister");
+const course = document.querySelector(".course");
+const branch = document.querySelector(".branch");
+const year = document.querySelector(".year");
+const semester = document.querySelector(".semester");
+const email = document.querySelector(".email");
+const rpasswords = document.querySelector(".rpassword");
+const login = document.querySelector(".loginsubmit");
 
-const registersubmit=document.querySelector('.register-submit');
-const form=document.querySelector('.form');
+const registersubmit = document.querySelector(".register-submit");
+const form = document.querySelector(".form");
 
-
-sname.addEventListener('input',function(e){
-
-   const snameuser=e.target.value;
-   const studentobj=JSON.parse(localStorage.getItem('student'));
-   if(studentobj.sname== null){
-      studentobj.sname=snameuser;
-      localStorage.setItem('student',JSON.stringify(studentobj));
-      firstregistered=true;
-   }
-   else{
-   firstregistered=false;
-   }
-
-  
-  
-});
-
-htno.addEventListener('input',function(e){
-
-    const htuser=e.target.value;
-    const studentobj=JSON.parse(localStorage.getItem('student'));
-    if(studentobj.htno==null){
-    studentobj.htno=htuser;
-    localStorage.setItem('student',JSON.stringify(studentobj));
-    }
-   
-   
- });
-  
- course.addEventListener('input',function(e){
-
-    const courseuser=e.target.value;
-    const studentobj=JSON.parse(localStorage.getItem('student'));
-    if(studentobj.course==null){
-    studentobj.course=courseuser;
-    localStorage.setItem('student',JSON.stringify(studentobj));
-    }
-    
-   
- });
-
- branch.addEventListener('input',function(e){
-  
-    const branchuser=e.target.value;
-    const studentobj=JSON.parse(localStorage.getItem('student'));
-    if(studentobj.branch==null){
-    studentobj.branch=branchuser;
-    localStorage.setItem('student',JSON.stringify(studentobj));
-    }
-   
-   
- });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
 
- year.addEventListener('input',function(e){
+  const studentobj = JSON.parse(localStorage.getItem("student"));
 
-    const yearuser=e.target.value;
-    const studentobj=JSON.parse(localStorage.getItem('student'));
-    if(studentobj.year==null)
-    {
-    studentobj.year=yearuser;
-    localStorage.setItem('student',JSON.stringify(studentobj));
-    }
-  
-   
- });
+  if (studentobj.sname == null) {
+    studentobj.sname = e.target.name.value;
+    localStorage.setItem("student", JSON.stringify(studentobj));
+    firstregistered = true;
+  } else {
+    firstregistered = false;
+  }
 
+  if (studentobj.htno == null) {
+    studentobj.htno = e.target.htr.value;
+    localStorage.setItem("student", JSON.stringify(studentobj));
+  }
 
- semester.addEventListener('input',function(e){
+  if (studentobj.course == null) {
+    studentobj.course = e.target.course.value;
+    localStorage.setItem("student", JSON.stringify(studentobj));
+  }
 
-    const semesteruser=e.target.value;
-    const studentobj=JSON.parse(localStorage.getItem('student'));
-    if(studentobj.semester==null){
-    studentobj.semester=semesteruser;
-    localStorage.setItem('student',JSON.stringify(studentobj));
-    }
-    
-   
- });
+  if (studentobj.branch == null) {
+    studentobj.branch = e.target.branch.value;
+    localStorage.setItem("student", JSON.stringify(studentobj));
+  }
 
+  if (studentobj.year == null) {
+    studentobj.year = e.target.year.value;
+    localStorage.setItem("student", JSON.stringify(studentobj));
+  }
 
- email.addEventListener('input',function(e){
+  if (studentobj.semester == null) {
+    studentobj.semester = e.target.semester.value;
+    localStorage.setItem("student", JSON.stringify(studentobj));
+  }
 
-    const emailuser=e.target.value;
-    const studentobj=JSON.parse(localStorage.getItem('student'));
-    if(studentobj.email==null){
-    studentobj.email=emailuser;
-    localStorage.setItem('student',JSON.stringify(studentobj));
-    }
-   
-   
- });
+  if (studentobj.email == null) {
+    studentobj.email = e.target.email.value;
+    localStorage.setItem("student", JSON.stringify(studentobj));
+  }
 
- rpasswords.addEventListener('input',function(e){
-  
-    const rpassworduser=e.target.value;
-    const studentobj=JSON.parse(localStorage.getItem('student'));
-    if(!studentobj.rpassword){
-    studentobj.rpassword=rpassworduser;
-    localStorage.setItem('student',JSON.stringify(studentobj));
-    }
-   
-  
-   
- });
+  if (studentobj.rpassword==null) {
+    studentobj.rpassword = e.target.password.value ;
+    localStorage.setItem("student", JSON.stringify(studentobj));
+  }
 
-
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    const studentobj=JSON.parse(localStorage.getItem('student'));
-    if(studentobj.sname && firstregistered==false){
-    const message=document.createElement('h3');
-    message.innerText="You have already registered on this device.Try to Login instead..";
-    message.style.color="brown";
-    const container=document.querySelector('.container');
+  if (studentobj.sname && firstregistered == false) {
+    const message = document.createElement("h3");
+    message.innerText =
+      "You have already registered on this device.Try to Login instead..";
+    message.style.color = "brown";
+    const container = document.querySelector(".container");
     container.appendChild(message);
-    }
-    else{
-    window.location="index.html";
-   }
-}
-)
-
-
-
-
+  } else {
+    window.location = "index.html";
+  }
+});
